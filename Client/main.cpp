99765e42e4a5562cc57bbe7a8abf6fd9675d132a
IAlmostDeveloper <kill2survive@yandex.ru>
2020-04-13 16:46:24 +0500

    register new types

--- Client/main.cpp
+++ Client/main.cpp
@@ -1,9 +1,13 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
-
+#include <QQmlContext>
+#include "Hotelsmodel.hpp"
+#include "Authmanager.hpp"
 int main(int argc, char *argv[])
 {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    qmlRegisterType<AuthManager>("io.qt.auth.manager", 1, 0, "AuthManager");
+    qmlRegisterType<HotelsModel>("io.qt.models.hotels", 1, 0, "HotelsModel");
 
     QGuiApplication app(argc, argv);
 
