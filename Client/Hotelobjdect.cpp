969ad04f9b7f371dec1b4830387ea60aad2ce07d
IAlmostDeveloper <kill2survive@yandex.ru>
2020-04-13 16:41:37 +0500

    added object for hotel data

--- Client/Hotelobjdect.cpp
+++ Client/Hotelobjdect.cpp
@@ -1,6 +1,58 @@
 #include "Hotelobjdect.hpp"
 
-HotelObjdect::HotelObjdect(QObject *parent) : QObject(parent)
+HotelObject::HotelObject(QObject *parent) : QObject(parent)
 {
 
+}
+
+HotelObject::HotelObject(const QString &name, const QString &address, int id, bool available)
+{
+    setName(name);
+    setAddress(address);
+    setId(id);
+    setAvailable(available);
+}
+
+QString HotelObject::name()
+{
+    return m_name;
+}
+
+QString HotelObject::address()
+{
+    return m_address;
+}
+
+int HotelObject::id()
+{
+    return m_id;
+}
+
+bool HotelObject::available()
+{
+    return m_available;
+}
+
+void HotelObject::setName(const QString &value)
+{
+    m_name = value;
+    emit nameChanged(value);
+}
+
+void HotelObject::setAddress(const QString &value)
+{
+    m_address = value;
+    emit addressChanged(value);
+}
+
+void HotelObject::setId(int value)
+{
+    m_id = value;
+    emit idChanged(value);
+}
+
+void HotelObject::setAvailable(bool value)
+{
+    m_available = value;
+    emit availableChanged(value);
 }
--- Client/Hotelobjdect.hpp
+++ Client/Hotelobjdect.hpp
@@ -3,15 +3,37 @@
 
 #include <QObject>
 
-class HotelObjdect : public QObject
+class HotelObject : public QObject
 {
     Q_OBJECT
+    Q_PROPERTY(QString name READ name WRITE setName NOTIFY nameChanged)
+    Q_PROPERTY(QString address READ address WRITE setAddress NOTIFY addressChanged)
+    Q_PROPERTY(int id READ id WRITE setId NOTIFY idChanged)
+    Q_PROPERTY(bool available READ available WRITE setAvailable NOTIFY availableChanged)
 public:
-    explicit HotelObjdect(QObject *parent = nullptr);
+    explicit HotelObject(QObject *parent = nullptr);
+    explicit HotelObject(const QString& name, const QString& address, int id, bool available);
+    QString name();
+    QString address();
+    int id();
+    bool available();
+
+    void setName(const QString& value);
+    void setAddress(const QString& value);
+    void setId(int value);
+    void setAvailable(bool value);
 
 signals:
+    void nameChanged(const QString& name);
+    void addressChanged(const QString& address);
+    void idChanged(int id);
+    void availableChanged(bool available);
 
-public slots:
+private:
+    QString m_name;
+    QString m_address;
+    int m_id;
+    bool m_available;
 };
 
 #endif // HOTELOBJDECT_HPP
