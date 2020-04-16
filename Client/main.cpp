f5effb27ad19ce3e75b3ae195c2786c6abe5c515
IAlmostDeveloper <kill2survive@yandex.ru>
2020-04-16 20:07:55 +0500

    Registered room model type

--- Client/main.cpp
+++ Client/main.cpp
@@ -1,13 +1,17 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 #include <QQmlContext>
-#include "Models/Hotelsmodel.hpp"
 #include "Models/Authmanager.hpp"
+#include "Models/Hotelsmodel.hpp"
+#include "Models/Roomsmodel.hpp"
+
 int main(int argc, char *argv[])
 {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     qmlRegisterType<AuthManager>("AuthManager", 1, 0, "AuthManager");
     qmlRegisterType<HotelsModel>("HotelsModel", 1, 0, "HotelsModel");
+    qmlRegisterType<RoomsModel>("RoomsModel", 1, 0, "RoomsModel");
+
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
